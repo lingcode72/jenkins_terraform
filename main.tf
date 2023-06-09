2911bcb5ff62685e26880624046615548c5ff179
lingcode72 <ling.curty.simplon@gmail.com>
2023-06-09 16:25:56 +0200

    main created

--- main.tf
+++ main.tf
@@ -12,9 +12,70 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id       = ""
+  client_secret   = ""
+  subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+  tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "lingResourceGroupJenkins"
-  location = "francecentral"
+  location = "West Europe"
 }
+
+resource   "azurerm_virtual_network"   "myvnet"   { 
+   name   =   "my-vnet" 
+   address_space   =   [ "10.0.0.0/16" ] 
+   location   =   azurerm_resource_group.rg.location
+   resource_group_name   =   azurerm_resource_group.rg.name 
+ } 
+
+ resource   "azurerm_subnet"   "frontendsubnet"   { 
+   name   =   "frontendSubnet" 
+   resource_group_name   =    azurerm_resource_group.rg.name 
+   virtual_network_name   =   azurerm_virtual_network.myvnet.name 
+   address_prefix   =   "10.0.1.0/24" 
+ } 
+
+ resource   "azurerm_public_ip"   "myvm1publicip"   { 
+   name   =   "pip1" 
+   location   =   azurerm_resource_group.rg.location
+   resource_group_name   =   azurerm_resource_group.rg.name 
+   allocation_method   =   "Dynamic" 
+   sku   =   "Basic" 
+ } 
+
+ resource   "azurerm_network_interface"   "myvm1nic"   { 
+   name   =   "myvm1-nic" 
+   location   =   azurerm_resource_group.rg.location
+   resource_group_name   =   azurerm_resource_group.rg.name 
+
+   ip_configuration   { 
+     name   =   "ipconfig1" 
+     subnet_id   =   azurerm_subnet.frontendsubnet.id 
+     private_ip_address_allocation   =   "Dynamic" 
+     public_ip_address_id   =   azurerm_public_ip.myvm 1 publicip.id 
+   } 
+ } 
+
+ resource   "azurerm_windows_virtual_machine"   "example"   { 
+   name                    =   "myvm1"   
+   location                =   azurerm_resource_group.rg.location
+   resource_group_name     =   azurerm_resource_group.rg.name 
+   network_interface_ids   =   [ azurerm_network_interface.myvm 1 nic.id ] 
+   size                    =   "Standard_B1s" 
+   admin_username          =   "adminuser" 
+   admin_password          =   "Password123!" 
+
+   source_image_reference   { 
+     publisher   =   "MicrosoftWindowsServer" 
+     offer       =   "WindowsServer" 
+     sku         =   "2019-Datacenter" 
+     version     =   "latest" 
+   } 
+
+   os_disk   { 
+     caching             =   "ReadWrite" 
+     storage_account_type   =   "Standard_LRS" 
+   } 
+ } 
