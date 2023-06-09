4fab6bd8723d139e09b75d3ed141715e2c993c79
lingcode72 <ling.curty.simplon@gmail.com>
2023-06-09 16:52:58 +0200

    main.tf modified

--- main.tf
+++ main.tf
@@ -12,70 +12,76 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = ""
-  client_secret   = ""
+  client_id       = "eaab451a-f58c-4e1f-bbb5-a42e6b4c4937"
+  client_secret   = "2~68Q~QbD1Plg3itKkzWdhdyp7-Rz_EtfbZB.ay9"
   subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
   tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
 }
 
+##Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "lingResourceGroupJenkins"
-  location = "West Europe"
+name = "lingResourceGroupJenkins"
+location = "france central"
 }
 
-resource   "azurerm_virtual_network"   "myvnet"   { 
-   name   =   "my-vnet" 
-   address_space   =   [ "10.0.0.0/16" ] 
-   location   =   azurerm_resource_group.rg.location
-   resource_group_name   =   azurerm_resource_group.rg.name 
- } 
+##Avaibility Set
+resource "azurerm_availability_set" "DemoAset" {
+name = "tf-aset"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+}
 
- resource   "azurerm_subnet"   "frontendsubnet"   { 
-   name   =   "frontendSubnet" 
-   resource_group_name   =    azurerm_resource_group.rg.name 
-   virtual_network_name   =   azurerm_virtual_network.myvnet.name 
-   address_prefix   =   "10.0.1.0/24" 
- } 
+##Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+name = "tf-vNet"
+address_space = ["10.0.0.0/16"]
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+}
 
- resource   "azurerm_public_ip"   "myvm1publicip"   { 
-   name   =   "pip1" 
-   location   =   azurerm_resource_group.rg.location
-   resource_group_name   =   azurerm_resource_group.rg.name 
-   allocation_method   =   "Dynamic" 
-   sku   =   "Basic" 
- } 
+##Subnet
+resource "azurerm_subnet" "subnet" {
+name = "Internal"
+resource_group_name = azurerm_resource_group.rg.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes = ["10.0.2.0/24"]
+}
 
- resource   "azurerm_network_interface"   "myvm1nic"   { 
-   name   =   "myvm1-nic" 
-   location   =   azurerm_resource_group.rg.location
-   resource_group_name   =   azurerm_resource_group.rg.name 
+##Network interface
+resource "azurerm_network_interface" "example" {
+name = "tf-vmwin-nic"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
 
-   ip_configuration   { 
-     name   =   "ipconfig1" 
-     subnet_id   =   azurerm_subnet.frontendsubnet.id 
-     private_ip_address_allocation   =   "Dynamic" 
-     public_ip_address_id   =   azurerm_public_ip.myvm 1 publicip.id 
-   } 
- } 
+ip_configuration {
+name = "internal"
+subnet_id = azurerm_subnet.subnet.id
+private_ip_address_allocation = "Dynamic"
+}
+}
 
- resource   "azurerm_windows_virtual_machine"   "example"   { 
-   name                    =   "myvm1"   
-   location                =   azurerm_resource_group.rg.location
-   resource_group_name     =   azurerm_resource_group.rg.name 
-   network_interface_ids   =   [ azurerm_network_interface.myvm 1 nic.id ] 
-   size                    =   "Standard_B1s" 
-   admin_username          =   "adminuser" 
-   admin_password          =   "Password123!" 
+##Azure Virtual Machine
+resource "azurerm_windows_virtual_machine" "example" {
+name = "tf-vmwin"
+resource_group_name = azurerm_resource_group.rg.name
+location = azurerm_resource_group.rg.location
+size = "Standard_F2"
+admin_username = "adminuser"
+admin_password = "P@$$w0rd1234!"
+availability_set_id = azurerm_availability_set.DemoAset.id
+network_interface_ids = [
+azurerm_network_interface.example.id,
+]
 
-   source_image_reference   { 
-     publisher   =   "MicrosoftWindowsServer" 
-     offer       =   "WindowsServer" 
-     sku         =   "2019-Datacenter" 
-     version     =   "latest" 
-   } 
+os_disk {
+caching = "ReadWrite"
+storage_account_type = "Standard_LRS"
+}
 
-   os_disk   { 
-     caching             =   "ReadWrite" 
-     storage_account_type   =   "Standard_LRS" 
-   } 
- } 
+source_image_reference {
+publisher = "MicrosoftWindowsServer"
+offer = "WindowsServer"
+sku = "2022-Datacenter"
+version = "latest"
+}
+}
